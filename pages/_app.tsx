4d0275f54fe46812ace17a87165ba45b6c8ce208
im-snowin <hello.snowin@gmail.com>
2022-12-22 03:27:17 +0530

    build a auth component for signin and signup.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,25 @@
 import type { AppProps } from "next/app";
+import { NextPageContext } from "next/types";
 import { ChakraProvider } from "@chakra-ui/react";
 import "reset-css";
 
 import theme from "../styles/theme";
 import Layout from "../components/Layout";
 
+interface CustomNextPageContext extends NextPageContext {
+  authPath: Boolean;
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Layout>
+      {Component.authPage ? (
         <Component {...pageProps} />
-      </Layout>
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
     </ChakraProvider>
   );
 }
