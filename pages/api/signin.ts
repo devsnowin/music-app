638952fade4972299a85e8611c8fb5be211207db
im-snowin <hello.snowin@gmail.com>
2022-12-22 01:35:34 +0530

    created singin and signup endpoints 🔒.

---
+++ pages/api/signin.ts
@@ -0,0 +1,47 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import cookie from "cookie";
+
+import prisma from "../../lib/prisma";
+
+const SECRET_KEY = process.env.JWT_SECRET || "cookies";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    const isValidPassword = user
+      ? await bcrypt.compare(password, user?.password)
+      : null;
+    if (user && isValidPassword) {
+      const token = jwt.sign(
+        { id: user.id, name: user.name, time: Date.now() },
+        SECRET_KEY,
+        { expiresIn: "8h" }
+      );
+
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("MUSIC_ACCESS_TOKEN", token, {
+          httpOnly: true,
+          maxAge: 8 * 60 * 60,
+          path: "/",
+          sameSite: "lax",
+          secure: process.env.NODE_ENV === "production",
+        })
+      );
+
+      return res.status(200).json({ data: user });
+    } else {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (e) {
+    console.log(e);
+    return res.status(404).json({ message: "user not found" });
+  }
+}
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -26,7 +26,7 @@ const run = async () => {
     })
   );
 
-  const user = prisma.user.upsert({
+  const user = await prisma.user.upsert({
     where: { email: "user@tem.com" },
     update: {},
     create: {
@@ -35,6 +35,23 @@ const run = async () => {
       password: await bcrypt.hash("123", 10),
     },
   });
+
+  const songs = await prisma.song.findMany({});
+  await Promise.all(
+    new Array(10).fill(1).map((_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist #${i + 1}`,
+          user: {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: songs.map((song) => ({ id: song.id })),
+          },
+        },
+      });
+    })
+  );
 };
 
 run()
