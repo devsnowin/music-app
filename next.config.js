a72c75d65692d06a310bf1800668dec7cad44c87
im-snowin <hello.snowin@gmail.com>
2022-12-24 18:17:03 +0530

    optimize the app and redeigned the components.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      },
-    ],
-  },
+	reactStrictMode: true,
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'images.pexels.com',
+			},
+		],
+	},
 };
 
 module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,23 @@
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { StoreProvider } from "easy-peasy";
-import "reset-css";
+import { ChakraProvider } from '@chakra-ui/react';
+import { StoreProvider } from 'easy-peasy';
+import 'reset-css';
 
-import { store } from "../lib/store";
-import theme from "../styles/theme";
-import Layout from "../components/Layout";
+import { store } from '../lib/store';
+import theme from '../styles/theme';
+import Layout from '../components/Layout';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider theme={theme}>
-      <StoreProvider store={store}>
-        {Component.authPage ? (
-          <Component {...pageProps} />
-        ) : (
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        )}
-      </StoreProvider>
-    </ChakraProvider>
-  );
+export default function App({ Component, pageProps }) {
+	return (
+		<ChakraProvider theme={theme}>
+			<StoreProvider store={store}>
+				{Component.authPage ? (
+					<Component {...pageProps} />
+				) : (
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+				)}
+			</StoreProvider>
+		</ChakraProvider>
+	);
 }
